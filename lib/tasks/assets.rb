8ede45004dc9fab5f582a71e3409f6aaf8cd8672
pandabrand <pandabrand@gmail.com>
2015-10-10 11:42:50 -0500

    debugging asset compile

--- lib/tasks/assets.rb
+++ lib/tasks/assets.rb
@@ -3,12 +3,12 @@ namespace :assets do
   task :precompile => :app do
     assets = Darksidetaco::Routes::Base.assets
     target = Pathname(Darksidetaco::App.root) + 'public/assets'
+    print 'assets to compile: ' + assets.size
 
     assets.each_logical_path do |logical_path|
       if asset = assets.find_asset(logical_path)
         filename = target.join(asset.digest_path)
         FileUtils.mkpath(filename.dirname)
-        puts filename
         asset.write_to(filename)
 
         filename = target.join(logical_path)
