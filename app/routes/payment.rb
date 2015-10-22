9fa15ae2fc154824233efb2388ff032b5bdea899
pandabrand <pandabrand@gmail.com>
2015-10-22 00:31:22 -0500

    redoing order page

--- app/routes/payment.rb
+++ app/routes/payment.rb
@@ -3,9 +3,10 @@ module Darksidetaco
     class Payment < Base
       post '/payment' do
         order = session[:order]
-        order[:phone] = params[:phone]
+        @phone = params[:phone]
+        @items = order[:items]
 		@order_items = Hash.new
-		order[:items].each{|id, qty| @order_items[Stripe::Product.retrieve(id)] = qty}
+		@items.each{|id, qty| @order_items[Stripe::Product.retrieve(id)] = qty}
 		@description = Array.new
 		@order_items.each{|item, qty| @description.push(item.name + ": "  + qty)}
 		@total = @order_items.keys.map{|product| product.skus.data.first.price * @order_items[product].to_i}.inject(0, &:+)
--- app/routes/thanks.rb
+++ app/routes/thanks.rb
@@ -7,16 +7,21 @@ module Darksidetaco
 		  :email => params[:stripeEmail],
 		  :card  => params[:stripeToken]
 		)
-
+		
+		#remove items not needed
+		order_str = params[:order_v]
+		puts "order: " + order_str.to_s
+		order_hash = Hash[*order_str.split(",")]
 		items = Array.new
-		session[:order][:order_items].each{|product,qty| items.push({:type => 'sku',:parent => product.skus.data.first.id, :quantity => qty})}
+		order_hash.each{|id, qty| items.push({:type => 'sku', :parent => Stripe::Product.retrieve(id).skus.data.first.id, :quantity => qty})}
+# 		session[:order][:order_items].each{|product,qty| items.push({:type => 'sku',:parent => product.skus.data.first.id, :quantity => qty})}
 		@order = Stripe::Order.create(
 		  :currency => 'usd',
 		  :customer => customer.id,
 		  :items => items,
 		  :shipping => {
 			:name => params[:stripeShippingName],
-			:phone => session[:order][:phone],
+			:phone => params[:phone],
 			:address => {
 			  :line1 => params[:stripeShippingAddressLine1],
 			  :city => params[:stripeShippingAddressCity],
