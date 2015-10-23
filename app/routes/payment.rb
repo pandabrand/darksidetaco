ff69dce68f6480f974dd17b0dd6f9472133f4b27
pandabrand <pandabrand@gmail.com>
2015-10-23 18:43:36 -0500

    ssl enforcer

--- app/routes/payment.rb
+++ app/routes/payment.rb
@@ -2,7 +2,7 @@ module Darksidetaco
   module Routes
     class Payment < Base
       post '/payment' do
-        order = session[:order]
+#         order = session[:order]
         @phone = params[:phone]
         @notes = params[:notes]
 #         puts "session order: " + order.to_s
@@ -12,8 +12,8 @@ module Darksidetaco
 		@description = Array.new
 		@order_items.each{|item, qty| @description.push(item.name + ": "  + qty)}
 		@total = @order_items.keys.map{|product| product.skus.data.first.price * @order_items[product].to_i}.inject(0, &:+)
-		order[:order_items] = @order_items
-		session[:order] = order
+# 		order[:order_items] = @order_items
+# 		session[:order] = order
       	erb :payment
       end
     end
